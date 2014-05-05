84de1253eed9f110b59d29126fee5acd4702dca4
Danyel Bayraktar <danyel@webhippie.de>
2014-05-05 22:49:12 +0200

    Initial commit

---
+++ desktop/src/de/fau/cs/mad/fly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.fau.cs.mad.fly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.fau.cs.mad.fly.Fly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fly(), config);
+	}
+}
---
+++ ios/src/de/fau/cs/mad/fly/IOSLauncher.java
@@ -0,0 +1,22 @@
+package de.fau.cs.mad.fly;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import de.fau.cs.mad.fly.Fly;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Fly(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
