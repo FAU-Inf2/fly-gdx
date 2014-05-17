75a7f03b92ee00b5a5a57f99471754d1095dbaca
Lukas Hahmann <lukas.hahmann@gmail.com>
2014-05-17 11:08:53 +0200

    created a nicer method to process the back key

--- core/src/de/fau/cs/mad/fly/LevelChooserScreen.java
+++ core/src/de/fau/cs/mad/fly/LevelChooserScreen.java
@@ -1,49 +1,59 @@
 package de.fau.cs.mad.fly;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Application.ApplicationType;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 /**
  * Offers a selections of Levels to start
- *
+ * 
  * @author Lukas Hahmann
  */
-public class LevelChooserScreen implements Screen, InputProcessor {
+public class LevelChooserScreen implements Screen {
 	private final Fly game;
-	
+
 	private SpriteBatch batch;
 	private Skin skin;
 	private Stage stage;
 	private Table table;
-	
+	private InputMultiplexer inputProcessor;
+
 	public LevelChooserScreen(final Fly game) {
 		this.game = game;
-		
+
 		batch = new SpriteBatch();
 		skin = new Skin(Gdx.files.internal("uiskin.json"));
-		
-		if(Gdx.app.getType() == ApplicationType.Android) {
-			stage = new Stage(new FillViewport(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f));
-		} else {
-			stage = new Stage(new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
-		}
-	}
-	
 
+		if (Gdx.app.getType() == ApplicationType.Android) {
+			stage = new Stage(new FillViewport(Gdx.graphics.getWidth() / 2f,
+					Gdx.graphics.getHeight() / 2f));
+		} else {
+			stage = new Stage(new FillViewport(Gdx.graphics.getWidth(),
+					Gdx.graphics.getHeight()));
+		}
+		inputProcessor = new InputMultiplexer();
+		InputProcessor backProcessor = new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+
+                if ((keycode == Keys.ESCAPE) || (keycode == Keys.BACK) ) {
+                	game.setMainMenuScreen();
+                }
+                return false;
+            }
+        };
+        inputProcessor.addProcessor(backProcessor);
+	}
 
 	@Override
 	public void render(float delta) {
@@ -62,93 +72,29 @@ public class LevelChooserScreen implements Screen, InputProcessor {
 	@Override
 	public void show() {
 		Gdx.input.setCatchBackKey(true);
-		Gdx.input.setInputProcessor(this);
+		Gdx.input.setInputProcessor(inputProcessor);
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-	}
-	
-	@Override
-	public boolean keyDown(int keycode) {
-		if (keycode == Keys.BACK || keycode == Keys.BACKSPACE) {
-			game.setMainMenuScreen();
-		}
-		return false;
-	}
-
-
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 }
