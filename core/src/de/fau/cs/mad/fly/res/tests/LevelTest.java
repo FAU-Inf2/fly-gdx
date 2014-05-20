3b0a67a40f1df59fb486e37a13b513188bff5565
Qufang Fan <qufang.fan@fau.de>
2014-05-20 22:27:22 +0200

    Define src, resources folders and test folders

---
+++ core/src/de/fau/cs/mad/fly/res/tests/LevelTest.java
@@ -0,0 +1,26 @@
+package de.fau.cs.mad.fly.res.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+
+import de.fau.cs.mad.fly.res.Level;
+
+public class LevelTest {
+	
+	private Level level = new Level();
+
+	@Test
+	public void testToString() {
+		System.out.println(level.toString());
+		System.out.println("There is nothing to test here.");
+	}
+	
+	@Test
+	public void testGetLevelBorder(){
+		//ModelInstance model = level.getLevelBorder();		
+	}
+	
+}
