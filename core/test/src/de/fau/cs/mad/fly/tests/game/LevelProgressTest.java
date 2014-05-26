8efbd43b9879221c7769abc6cf0c1957a42bb741
Lukas Hahmann <lukas.hahmann@gmail.com>
2014-05-26 23:17:02 +0200

    extended LevelPrgressTest to manage a non linear level

--- core/test/src/de/fau/cs/mad/fly/tests/game/LevelProgressTest.java
+++ core/test/src/de/fau/cs/mad/fly/tests/game/LevelProgressTest.java
@@ -55,10 +55,54 @@ public class LevelProgressTest {
 	@Test
 	public void testGatePassed() {
 		Gate testgate = testLevel.gates.get(0);
+
+		// passing first gate
 		testProgress.gatePassed(testgate);
-		assertNotSame("", testgate, testProgress.getNextGates().get(0));
+		assertNotSame(
+				"When passing a valid gate, a new gate should be the next gate.",
+				testgate, testProgress.getNextGates().get(0));
 		assertEquals(
-				"The list of next gates should only contain one Gate after the first gate",
+				"The list of next gates should only contain one gate after the first gate",
+				1, testProgress.getNextGates().size());
+
+		// passing second gate
+		testgate = testLevel.gates.get(1);
+		testProgress.gatePassed(testgate);
+		assertNotSame(
+				"When passing a valid gate, a new gate should be the next gate.",
+				testgate, testProgress.getNextGates().get(0));
+		assertEquals(
+				"The list of next gates should only contain one gate after the second gate",
+				1, testProgress.getNextGates().size());
+
+		// passing third gate
+		testgate = testLevel.gates.get(2);
+		testProgress.gatePassed(testgate);
+		assertNotSame(
+				"When passing a valid gate, a new gate should be the next gate.",
+				testgate, testProgress.getNextGates().get(0));
+		assertEquals(
+				"The list of next gates should only contain two gates after the third gate",
+				2, testProgress.getNextGates().size());
+
+		// passing fourth gate
+		testgate = testLevel.gates.get(3);
+		testProgress.gatePassed(testgate);
+		assertNotSame(
+				"When passing a valid gate, a new gate should be the next gate.",
+				testgate, testProgress.getNextGates().get(0));
+		assertEquals(
+				"The list of next gates should only contain two gates after the fourth gate",
+				2, testProgress.getNextGates().size());
+		
+		// passing last gate
+		testgate = testLevel.gates.get(5);
+		testProgress.gatePassed(testgate);
+		assertNotSame(
+				"When passing a valid gate, a new gate should be the next gate.",
+				testgate, testProgress.getNextGates().get(0));
+		assertEquals(
+				"The list of next gates should only contain two gates after the fourth gate",
 				1, testProgress.getNextGates().size());
 	}
 
