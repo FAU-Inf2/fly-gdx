ecb630aecd496452467a8b0c9a63036a881d097a
Lukas Hahmann <lukas.hahmann@gmail.com>
2014-05-28 14:08:32 +0200

    created basic classes for gate indicator

---
+++ core/src/de/fau/cs/mad/fly/game/GateIndcator.java
@@ -0,0 +1,37 @@
+package de.fau.cs.mad.fly.game;
+
+/**
+ * This class implements the function to show in the game small arrows that
+ * indicate the direction of the next gates.
+ * 
+ * @author Lukas Hahmann
+ * 
+ */
+public class GateIndcator implements IFeatureInit, IFeatureFinishLevel,
+		IFeatureRender, IFeatureGatePassed {
+
+	@Override
+	public void render(GameController game, float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void finish(GameController game) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GameController game) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void gatePassed(GameController gameController) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
