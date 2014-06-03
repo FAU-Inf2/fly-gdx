8008b5aa5fc0a2b8f572bd037e89dc7098a03074
Lukas Hahmann <lukas.hahmann@gmail.com>
2014-06-03 09:32:56 +0200

    refactoring

--- core/src/de/fau/cs/mad/fly/game/GateIndicator.java
+++ core/src/de/fau/cs/mad/fly/game/GateIndicator.java
@@ -1,21 +1,12 @@
 package de.fau.cs.mad.fly.game;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
-import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 
 import de.fau.cs.mad.fly.Assets;
-import de.fau.cs.mad.fly.features.IFeatureFinishLevel;
-import de.fau.cs.mad.fly.features.IFeatureDispose;
 import de.fau.cs.mad.fly.features.IFeatureFinish;
 import de.fau.cs.mad.fly.features.IFeatureGatePassed;
 import de.fau.cs.mad.fly.features.IFeatureInit;
@@ -29,17 +20,15 @@ import de.fau.cs.mad.fly.res.Gate;
  * @author Lukas Hahmann
  * 
  */
-public class GateIndicator implements IFeatureInit, IFeatureFinishLevel,
+public class GateIndicator implements IFeatureInit, IFeatureFinish,
 		IFeatureRender, IFeatureGatePassed {
 
 	private ModelInstance arrowModel;
 	/** visualize current target point */
-	private ModelInstance cube;
 	private GameController gameController;
 	private ModelBuilder modelBuilder;
 	private ModelBatch batch;
 
-	private ModelInstance debugModel;
 
 	@Override
 	public void init(GameController game) {
@@ -47,20 +36,12 @@ public class GateIndicator implements IFeatureInit, IFeatureFinishLevel,
 		Assets.loadArrow();
 		arrowModel = new ModelInstance(Assets.manager.get(Assets.arrow));
 		batch = game.batch;
-		
-		//TODO: remove this if arrow is working
-		this.modelBuilder = new ModelBuilder();
-		cube = new ModelInstance(modelBuilder.createBox(0.1f, 0.1f, 0.1f,
-				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-				Usage.Position | Usage.Normal));
-		cube.transform.setToTranslation(1, 1, 10);
 	}
 
 
 	@Override
 	public void render(float delta) {
 		Vector3 targetPosition = new Vector3(1, 1, 10);
-		Vector3 cameraPosition = gameController.getCamera().position.cpy();
 		Vector3 cameraDirection = gameController.getCamera().direction.cpy();
 		Vector3 up = gameController.getCamera().up.cpy();
 		Vector3 down = up.cpy().scl(-1);
@@ -92,8 +73,6 @@ public class GateIndicator implements IFeatureInit, IFeatureFinishLevel,
 				.trn(gatePositionRelativeToCamera);
 
 		batch.render(arrowModel);
-		batch.render(cube);
-		//batch.render(debugModel);
 	}
 
 	@Override
