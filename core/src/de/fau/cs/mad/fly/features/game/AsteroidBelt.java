9d4a2ee56cb12980ca681a1d3743f9462feb40a6
Tobias Zangl <t.zangl@web.de>
2014-06-22 01:04:14 +0200

    added random moving and rotation to the asteroids, added check if the astroid is created too close to another object

--- core/src/de/fau/cs/mad/fly/features/game/AsteroidBelt.java
+++ core/src/de/fau/cs/mad/fly/features/game/AsteroidBelt.java
@@ -19,6 +19,7 @@ import de.fau.cs.mad.fly.game.CollisionDetector;
 import de.fau.cs.mad.fly.game.GameController;
 import de.fau.cs.mad.fly.game.GameModel;
 import de.fau.cs.mad.fly.game.GameObject;
+import de.fau.cs.mad.fly.game.MovingGameObject;
 import de.fau.cs.mad.fly.res.Assets;
 
 /**
@@ -36,8 +37,14 @@ public class AsteroidBelt implements IFeatureLoad, IFeatureInit, IFeatureRender,
 	
 	private int asteroidCount = 0;
 	private String modelRef;
-	private List<GameObject> asteroids;
+	private List<MovingGameObject> asteroids;
 
+	/**
+	 * Constructor for the AstroidBelt
+	 * @param asteroidCount		The count of asteroids to display and update.
+	 * @param modelRef			The model used for the asteroids.
+	 * @param levelSize			The size of the box where the asteroids have to be created inside.
+	 */
 	public AsteroidBelt(int asteroidCount, String modelRef, Vector3 levelSize) {
 		this.asteroidCount = asteroidCount;
 		this.modelRef = modelRef;
@@ -50,7 +57,7 @@ public class AsteroidBelt implements IFeatureLoad, IFeatureInit, IFeatureRender,
 		batch = gameController.getBatch();
 		environment = gameController.getLevel().getEnvironment();
 		
-		asteroids = new ArrayList<GameObject>(asteroidCount);
+		asteroids = new ArrayList<MovingGameObject>(asteroidCount);
 
 		GameModel model = gameController.getLevel().getDependency(modelRef);
 		if(model == null) {
@@ -61,31 +68,106 @@ public class AsteroidBelt implements IFeatureLoad, IFeatureInit, IFeatureRender,
 		Matrix4 identityMatrix = new Matrix4();
 		
 		for(int i = 0; i < asteroidCount; i++) {
-			GameObject asteroid = new GameObject(model);
-			
-			asteroid.transform.setToTranslation(getRandomVector(levelSize));
-			//asteroid.movingDir = getRandomVector(new Vector3(2.0f, 2.0f, 2.0f)).nor();
-			//asteroid.movingSpeed = 1.0f;
 
-			btCollisionShape shape = gameController.getCollisionDetector().getShapeManager().createConvexShape(modelRef, asteroid);
-			asteroid.filterGroup = CollisionDetector.OBJECT_FLAG;
-			asteroid.filterMask = CollisionDetector.ALL_FLAG;
-			asteroid.setCollisionObject(shape);
-			gameController.getCollisionDetector().addCollisionObject(asteroid);
-
-			asteroids.add(asteroid);
+			asteroids.add(createAsteroid(model));
 		}
 
 		Gdx.app.log("AsteroidBelt.load", "Asteroid belt created.");
 	}
 	
+	/**
+	 * Creates an asteroid with the given model.
+	 * <p>
+	 * Calculates moving and rotation randomly between specific values after cheking if the position is not colling with anything else.
+	 * Creates the collision object of the asteroid.
+	 * @param model			The model used for the asteroid.
+	 * @return
+	 */
+	private MovingGameObject createAsteroid(GameModel model) {
+		MovingGameObject asteroid = new MovingGameObject(model);
+		
+		Vector3 position;
+		do {
+			position = getRandomVector(levelSize);
+		} while(checkForSpawnCollision(position, asteroids, 40.0f));
+		
+		asteroid.transform.setToTranslation(position);
+		asteroid.setRotationVector(getRandomVector(new Vector3(2.0f, 2.0f, 2.0f)).nor());
+		asteroid.setRotationSpeed(getRandomFloat(2.0f, 10.0f));
+		asteroid.setMovingVector(getRandomVector(new Vector3(2.0f, 2.0f, 2.0f)).nor());
+		asteroid.setMovingSpeed(getRandomFloat(2.0f, 10.0f));
+
+		btCollisionShape shape = gameController.getCollisionDetector().getShapeManager().createConvexShape(modelRef, asteroid);
+		asteroid.filterGroup = CollisionDetector.OBJECT_FLAG;
+		asteroid.filterMask = CollisionDetector.ALL_FLAG;
+		asteroid.setCollisionObject(shape);
+		gameController.getCollisionDetector().addCollisionObject(asteroid);
+
+		return asteroid;
+	}
+	
+	/**
+	 * Checks if the calculated position is colliding with an already created asteroid or any other component of the level.
+	 * @param position		The currently calculated position.
+	 * @param asteroids		The list of the already created asteroids.
+	 * @param distance2		The minimum distance between a created asteroid and the other objects.
+	 * @return
+	 */
+	private boolean checkForSpawnCollision(Vector3 position, List<MovingGameObject> asteroids, float distance2) {
+		for(MovingGameObject asteroid : asteroids) {
+			if(position.dst2(asteroid.getPosition()) < distance2) {
+				Gdx.app.log("AsteroidBelt.checkForSpawnCollision", "SpawnCollision with other asteroid");
+				return true;
+			}
+		}
+		
+		for(GameObject component : gameController.getLevel().components) {
+			if(position.dst2(component.getPosition()) < distance2) {
+				Gdx.app.log("AsteroidBelt.checkForSpawnCollision", "SpawnCollision with level component");
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Calculates a random vector within a given size.
+	 * @param size		The maximum size of the random vector.
+	 * @return random vector.
+	 */
 	private Vector3 getRandomVector(Vector3 size) {
 		Vector3 v = new Vector3();
 		v.x = (float) (Math.random() * size.x) - size.x / 2.0f;
 		v.y = (float) (Math.random() * size.y) - size.y / 2.0f;
 		v.z = (float) (Math.random() * size.z) - size.z / 2.0f;
-		System.out.println(v.x + " " + v.y + " " + v.z);
+		//System.out.println(v.x + " " + v.y + " " + v.z);
 		return v;
+	}
+	
+	/**
+	 * Calculates a random floating point number between min and max values.
+	 * @param min		The minimum value of the floating point number.
+	 * @param max		The maximum value of the floating point number.
+	 * @return random floating point number.
+	 */
+	private float getRandomFloat(float min, float max) {
+		float speed = (float) (Math.random() * (max-min)) + min;
+		return speed;
+	}
+	
+	/**
+	 * Checks if the asteroid is too far away of a given position.
+	 * @param asteroid		The asteroid to check.
+	 * @param target		The target position to check.
+	 * @param distance2		The minimum distance between the astroid and the target.
+	 * @return true, if it is too far away, otherwise false.
+	 */
+	private boolean checkOutOfBound(MovingGameObject asteroid, Vector3 target, float distance2) {
+		if(asteroid.getPosition().dst2(target) > distance2) {
+			return true;
+		}
+		return false;
 	}
 	
 	@Override
@@ -96,9 +178,14 @@ public class AsteroidBelt implements IFeatureLoad, IFeatureInit, IFeatureRender,
 	@Override
 	public void render(float delta) {
 		batch.begin(camera);
-		for(GameObject asteroid : asteroids) {
-			//asteroid.transform.
+		for(MovingGameObject asteroid : asteroids) {
+			asteroid.rotate(delta);
 			
+			if(checkOutOfBound(asteroid, gameController.getCamera().position, 2000.0f)) {
+				asteroid.flipDirection();
+			}
+			
+			asteroid.move(delta);
 			asteroid.render(batch, environment, camera);
 		}
 		batch.end();
