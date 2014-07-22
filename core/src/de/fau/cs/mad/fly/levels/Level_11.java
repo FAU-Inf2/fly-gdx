e66f86e190137222dccd2cff19b2a2ad10cd6753
Tobias Zangl <t.zangl@web.de>
2014-07-22 15:16:41 +0200

    integer time listener now as possible interface for the features and the levels

--- core/src/de/fau/cs/mad/fly/levels/Level_11.java
+++ core/src/de/fau/cs/mad/fly/levels/Level_11.java
@@ -1,11 +1,13 @@
 package de.fau.cs.mad.fly.levels;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector3;
 
 import de.fau.cs.mad.fly.features.game.FlyingObjects;
 import de.fau.cs.mad.fly.features.upgrades.AddTimeUpgrade;
 import de.fau.cs.mad.fly.features.upgrades.InstantSpeedUpgrade;
 import de.fau.cs.mad.fly.game.GameControllerBuilder;
+import de.fau.cs.mad.fly.game.IntegerTimeListener;
 
 /**
  * Level script file for level 11.
@@ -15,11 +17,13 @@ import de.fau.cs.mad.fly.game.GameControllerBuilder;
  * @author Tobi
  *
  */
-public class Level_11 implements ILevel {
+public class Level_11 implements ILevel, IntegerTimeListener {
 
 	@Override
 	public void create(GameControllerBuilder builder) {
-		FlyingObjects asteroidBelt = new FlyingObjects("asteroid", 100, "asteroid", new Vector3(20.0f, 20.0f, 20.0f));
+		builder.addFeatureToLists(this);
+		
+		FlyingObjects asteroidBelt = new FlyingObjects("asteroid", 10, "asteroid", new Vector3(20.0f, 20.0f, 20.0f));
 		builder.addFeatureToLists(asteroidBelt);
 		
 		InstantSpeedUpgrade instantSpeedUpgrade = new InstantSpeedUpgrade("speedUpgrade", 4.0f, 10.0f);
@@ -27,5 +31,10 @@ public class Level_11 implements ILevel {
 		
 		AddTimeUpgrade addTimeUpgrade = new AddTimeUpgrade("timeUpgrade", 10.0f);
 		builder.addFeatureToLists(addTimeUpgrade);
+	}
+
+	@Override
+	public void integerTimeChanged(int newTime) {
+		Gdx.app.log("Level_11", "Time: " + newTime);
 	}
 }
