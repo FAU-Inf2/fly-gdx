4c9b3785ef7164ab97cd29d7423b8d34d5d52c14
Tobias Zangl <t.zangl@web.de>
2014-08-05 02:04:16 +0200

    added loading of levels in different folder, sorting by id and new level 11 json

--- core/src/de/fau/cs/mad/fly/profile/LevelManager.java
+++ core/src/de/fau/cs/mad/fly/profile/LevelManager.java
@@ -1,6 +1,7 @@
 package de.fau.cs.mad.fly.profile;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
@@ -34,20 +35,50 @@ public class LevelManager {
 
 	private List<Level.Head> levels = null;
 
+	/**
+	 * Creates the level list by parsing the level directory if its not already created.
+	 * 
+	 * @return sorted list of levels by id.
+	 */
 	public List<Level.Head> getLevelList() {
 		if (levels == null) {
 			levels = new ArrayList<Level.Head>();
 			FileHandle dirHandle = Gdx.files.internal("levels/");
-			for (FileHandle file : dirHandle.list()) {
-				JsonValue json = reader.parse(file);
+			readLevelDirectories(levels, dirHandle);
+			levels.sort(new Comparator<Level.Head>() {
+				@Override
+				public int compare(Level.Head first, Level.Head second){
+				    if(first.id < second.id) {
+				    	return -1;
+				    } else if(first.id > second.id) {
+				    	return 1;
+				    }
+				    return 0;
+				}
+		    });
+		}
+		return levels;
+	}
+	
+	/**
+	 * Recursively parses the level directory and stores the levels in the list.
+	 * 
+	 * @param levels		The list to store the levels.
+	 * @param dirHandle		The handle to the current directory.
+	 */
+	private void readLevelDirectories(List<Level.Head> levels, FileHandle dirHandle) {
+		for (FileHandle handle : dirHandle.list()) {
+			if(handle.isDirectory()) {
+				readLevelDirectories(levels, handle);
+			} else {
+				JsonValue json = reader.parse(handle);
 				Level.Head levelHead = new Level.Head();
 				levelHead.name = json.getString("name");
 				levelHead.id = json.getInt("id");
-				levelHead.file = file;
+				levelHead.file = handle;
 				levels.add(levelHead);
 			}
 		}
-		return levels;
 	}
 
 	public String getLevelName(int levelID) {
