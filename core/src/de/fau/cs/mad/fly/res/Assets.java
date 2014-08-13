c07d8a0cee117ecd162be00386bcca3357b97750
tobiza <t.zangl@web.de>
2014-08-13 02:00:47 +0200

    removed unnecessary asset descriptors

--- core/src/de/fau/cs/mad/fly/res/Assets.java
+++ core/src/de/fau/cs/mad/fly/res/Assets.java
@@ -2,11 +2,8 @@ package de.fau.cs.mad.fly.res;
 
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
 
 import de.fau.cs.mad.fly.game.GameModel;
 
@@ -17,17 +14,14 @@ import de.fau.cs.mad.fly.game.GameModel;
  */
 public class Assets {
     public static AssetManager manager;
-    
-    public static final AssetDescriptor<FreeTypeFontGenerator> font = new AssetDescriptor<FreeTypeFontGenerator>("OpenSans-Regular.ttf", FreeTypeFontGenerator.class);
+
     public static final AssetDescriptor<TextureAtlas> textureAtlas = new AssetDescriptor<TextureAtlas>("uiskin.atlas", TextureAtlas.class);
     
     public static final AssetDescriptor<GameModel> arrow = new AssetDescriptor<GameModel>("3d/arrow/arrow", GameModel.class);
     public static final AssetDescriptor<Texture> background = new AssetDescriptor<Texture>("background.png", Texture.class);
-    public static final AssetDescriptor<GameModel> spaceship = new AssetDescriptor<GameModel>("spaceship", GameModel.class);
     
     public static void init() {
         manager = new AssetManager();
-        manager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(new InternalFileHandleResolver()));
         manager.setLoader(Level.class, new LevelLoader());
         manager.setLoader(GameModel.class, new GameModelLoader());
         manager.finishLoading();
