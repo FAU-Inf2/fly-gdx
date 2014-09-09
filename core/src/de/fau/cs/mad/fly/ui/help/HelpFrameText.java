0398c95d7998308431e4babc3e13f33cc555e3b3
Lukas Hahmann <tfr.didi@gmail.com>
2014-09-09 15:35:18 +0200

    reduced the loading of the main menu screen by about 30%, by lazy loading of the help content (170 -> 120 ms)

--- core/src/de/fau/cs/mad/fly/ui/help/HelpFrameText.java
+++ core/src/de/fau/cs/mad/fly/ui/help/HelpFrameText.java
@@ -3,9 +3,9 @@ package de.fau.cs.mad.fly.ui.help;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 import de.fau.cs.mad.fly.I18n;
@@ -19,12 +19,32 @@ import de.fau.cs.mad.fly.ui.UI;
  */
 public class HelpFrameText extends HelpFrame {
     
+    /** skin that is used for the label and the background */
+    private Skin skin;
+    /** local copy of the label text */
+    private String text;
+    /** flat that indicates whether this object is initialized */
+    private boolean initialized = false;
+    
     /**
-     * Create the content of the frame, a describing text and an arrow.
+     * Init the members.
+     * <p>
+     * The actual initialization is done in {@link #initialize}. This is used to
+     * reduce the initialization time of an object {@link HelpFrameText}.
      * 
      * @param skin
      */
     public HelpFrameText(Skin skin, String text) {
+        this.skin = skin;
+        this.text = text;
+    }
+    
+    /**
+     * Setup batch and stage, create label, table with background and the arrow.
+     * This is done to reduce the initialization time of an object
+     * {@link HelpFrameText}.
+     */
+    private void initialize() {
         super.setupBatchAndStage();
         
         LabelStyle labelStyle = skin.get("black", LabelStyle.class);
@@ -50,6 +70,10 @@ public class HelpFrameText extends HelpFrame {
     
     @Override
     public void render() {
+        if (!initialized) {
+            initialize();
+            initialized = true;
+        }
         stage.draw();
     }
     
