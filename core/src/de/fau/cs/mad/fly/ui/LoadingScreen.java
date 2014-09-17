484f285377d2c8b4a8647f14a7b0fe0baede0263
Lukas Hahmann <tfr.didi@gmail.com>
2014-09-17 11:04:41 +0200

    create new font, created new button style, used background for all screens

--- core/src/de/fau/cs/mad/fly/ui/LoadingScreen.java
+++ core/src/de/fau/cs/mad/fly/ui/LoadingScreen.java
@@ -3,9 +3,6 @@ package de.fau.cs.mad.fly.ui;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -15,7 +12,6 @@ import de.fau.cs.mad.fly.Fly;
 import de.fau.cs.mad.fly.I18n;
 import de.fau.cs.mad.fly.Loadable;
 import de.fau.cs.mad.fly.ProgressListener;
-import de.fau.cs.mad.fly.res.Assets;
 
 /**
  * Displays the splash screen.
@@ -23,8 +19,6 @@ import de.fau.cs.mad.fly.res.Assets;
  * @author Tobias Zangl, Lukas Hahmann <lukas.hahmann@gmail.com>
  */
 public class LoadingScreen<T> extends BasicScreen {
-    protected final SpriteBatch batch;
-    protected final Sprite background;
     protected ScalableProgressBar progressBar;
     protected final float progressBarWidth = 2000f;
     protected final TextButton button;
@@ -34,26 +28,8 @@ public class LoadingScreen<T> extends BasicScreen {
     protected final Loadable<T> listenable;
     
     public LoadingScreen(Loadable<T> listenable) {
-        batch = new SpriteBatch();
         this.listenable = listenable;
-        Assets.load(Assets.background);
-        background = new Sprite(Assets.manager.get(Assets.background));
         table = new Table();
-        // Here the current display size should be used, to make sure the image
-        // is not stretched
-        float xSkalingFactor = Gdx.graphics.getWidth() / background.getWidth();
-        float ySkalingFactor = Gdx.graphics.getHeight() / background.getHeight();
-        float deltaX = 0f;
-        float deltaY = 0f;
-        background.setOrigin(0, 0);
-        if (xSkalingFactor >= ySkalingFactor) {
-            background.setScale(xSkalingFactor);
-            deltaY = (Gdx.graphics.getHeight() - background.getHeight() * xSkalingFactor) / 2.0f;
-        } else {
-            background.setScale(ySkalingFactor);
-            deltaX = (Gdx.graphics.getWidth() - background.getWidth() * ySkalingFactor) / 2.0f;
-        }
-        background.setPosition(deltaX, deltaY);
         
         progressBar = new ScalableProgressBar(skin);
         progressBar.setWidth(progressBarWidth);
@@ -93,17 +69,8 @@ public class LoadingScreen<T> extends BasicScreen {
     
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
         listenable.update();
-        
-        batch.begin();
-        background.draw(batch);
-        batch.end();
-        
-        stage.act(delta);
-        stage.draw();
+        super.render(delta);
     }
     
     @Override
