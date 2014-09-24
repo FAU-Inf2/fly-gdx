c06ff8c5cf7b292fbfeda9e2c5758573274b4aa9
Lukas Hahmann <tfr.didi@gmail.com>
2014-09-25 01:02:36 +0200

    added renamed file, removed fly id from player screen to get space for back button

---
+++ core/src/de/fau/cs/mad/fly/ui/DialogWithOneButton.java
@@ -0,0 +1,27 @@
+package de.fau.cs.mad.fly.ui;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class DialogWithOneButton extends Dialog {
+    
+    /**
+     * this object is passed to {@link #result(Object)} when the button is
+     * pressed
+     */
+    public static final String FIRST_BUTTON = "ok";
+    
+    public DialogWithOneButton(String text, String buttonText) {
+        super("", SkinManager.getInstance().getSkin(), "dialog");
+        super.text(text).pad(UI.Dialogs.PADDING);
+        TextButton button = new TextButton(buttonText, SkinManager.getInstance().getSkin());
+        super.getButtonTable().add(button).pad(UI.Dialogs.PADDING);
+        super.setObject(button, FIRST_BUTTON);
+        super.key(Keys.ENTER, FIRST_BUTTON);
+    }
+    
+    protected void result(Object object) {
+        super.hide();
+    }
+}
